c55a297700b7de70cc09a3bb451ca688682771ad
lyj <lyj@diquest.com>
2022-05-24 08:24:50 +0900

    feat: 유저 입력을 받는 View 구현

---
+++ src/main/java/bowling/BowlingGameApp.java
@@ -0,0 +1,10 @@
+package bowling;
+
+import bowling.view.GameInputView;
+
+public class BowlingGameApp {
+    public static void main(String[] args) {
+        GameInputView gameInputView = GameInputView.getInstance();
+        System.out.println(gameInputView.getUserName());
+    }
+}
---
+++ src/main/java/bowling/view/GameInputView.java
@@ -0,0 +1,26 @@
+package bowling.view;
+
+import java.util.Scanner;
+
+public class GameInputView {
+    private static final String USER_INPUT_MSG = "플레이어 이름은(3 english letters)?: ";
+    private static GameInputView instance;
+    private final Scanner scanner;
+
+    private GameInputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+
+    public static GameInputView getInstance() {
+        if (instance == null) {
+            instance = new GameInputView();
+        }
+        return instance;
+    }
+
+    public String getUserName() {
+        System.out.print(USER_INPUT_MSG);
+        return scanner.nextLine();
+    }
+}
